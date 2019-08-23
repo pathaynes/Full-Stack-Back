63b00d7bc838ecac94b01c244bbf3c230ae87947
Pat Haynes <pathaynes32@gmail.com>
2019-08-23 16:27:44 -0700

    added Form js and APP js as well as loading js and git

--- Public/src/services/tmnt-api.js
+++ Public/src/services/tmnt-api.js
@@ -7,3 +7,27 @@ export function getTurtles() {
     return fetch(url)
         .then(response => response.json());
 }
+
+export function getCharacter(id) {
+    const url = `${URL}/turtles/${id}`;
+    return fetch(url)
+        .then(response => response.json());
+} 
+
+export function addCharacter(character) {
+    const url = `${URL}/turtles`;
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',  
+        },
+        body: JSON.stringify(character)
+    })
+        .then(response => response.json());
+}
+
+export function getTypes() {
+    const url = `${URL}/types`;
+    return fetch(url)
+        .then(reponse => response.json());
+}
